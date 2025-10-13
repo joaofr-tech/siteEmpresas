c96429aaadb6c3a831c61867080b59ed635312ce
João Francisco <JF756061@gmail.com>
2025-10-13 10:34:58 -0300

    estrutura css e html

---
+++ scripts.js
@@ -0,0 +1,37 @@
+const items = [
+    {
+        "id": 1,
+        "imagem": "/public/img/weg.svg",
+        "nome": "WEG",
+        "ramo": "Bens de Capital",
+        "descricao": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ducimus id, amet deserunt, libero mollitia nobis commodi optio corporis a exercitationem dolorem voluptatum!"
+    },
+    {
+        "id": 2,
+        "imagem": "/public/img/petrobras.svg",
+        "nome": "Petrobras",
+        "ramo": "Petrolifera",
+        "descricao": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ducimus id, amet deserunt, libero mollitia nobis commodi optio corporis a exercitationem dolorem voluptatum!"
+    },
+    {
+        "id": 3,
+        "imagem": "/public/img/o-boticario.svg",
+        "nome": "O Boticario",
+        "ramo": "Perfumaria",
+        "descricao": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ducimus id, amet deserunt, libero mollitia nobis commodi optio corporis a exercitationem dolorem voluptatum!"
+    },
+    {
+        "id": 4,
+        "imagem": "/public/img/renner.svg",
+        "nome": "Renner",
+        "ramo": "Moda",
+        "descricao": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ducimus id, amet deserunt, libero mollitia nobis commodi optio corporis a exercitationem dolorem voluptatum!"
+    },
+    {
+        "id": 5,
+        "imagem": "/public/img/ambev.svg",
+        "nome": "Ambev",
+        "ramo": "Cervejaria",
+        "descricao": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ducimus id, amet deserunt, libero mollitia nobis commodi optio corporis a exercitationem dolorem voluptatum!"
+    },
+]
